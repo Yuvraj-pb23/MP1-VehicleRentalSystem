6b9c2e05cb7ff783a38f6e3ec4fd62f85dc61aa9
yuvraj <yuvrajjabbal@gmail.com>
2024-02-05 18:30:56 +0530

    new

---
+++ user.h
@@ -0,0 +1,33 @@
+#ifndef users_h
+#define users_h
+
+#include "storable.cpp"
+#include  <string>
+
+using namespace std;
+
+class User : public Storable
+{
+    private:
+
+    string name;
+    string contact;
+    string email;
+
+    public:
+
+    User (string name, string contact, string email, long recordId = 0);
+
+    string getName ( ) const;
+    string getContact ( ) const;
+    string gerEmail ( ) const;
+
+    void setName (string newName);
+    void setContact (string newContact);
+    void setEmail ( string newEmail);
+    void display ( ) const;
+    string toString ( ) const;
+    void setDataFrom(Storable * s);
+};
+
+#endif
