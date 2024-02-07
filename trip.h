350f47909ee08d481f431c3c1c2a080a73d1d744
yuvraj <yuvrajjabbal@gmail.com>
2024-02-07 15:37:02 +0530

    new

---
+++ trip.h
@@ -0,0 +1,51 @@
+#ifndef trip_h
+#define trip_h
+
+#include "date.h"
+#include "storable.cpp"
+#include "user.h"
+#include "vehicle.h"
+#include <string>
+
+using namespace std;
+
+class Trip : public Storable
+{
+    private:
+    
+    const Vehicle * vehicle;
+    const User * user;
+    
+    Date startDate;
+    Date endDate;
+
+    long startReading;
+    long endReading;
+
+    dauble fare;
+    bool completed;
+
+    public:
+    
+    Trip (const Vehicle * vehicle, const User * user, Date startDate, Date endDate, long recordId = 0,
+    long startReading = 0, long endReading = 0, double fare = 0.0, bool isCompeted = false );
+
+    const User & getUser ( ) const;
+    const Vehicle & getVehicle ( ) const;
+    Date getStartDate ( ) const;
+    Date getEndDate( ) const;
+    long getStartReading( ) const;
+    long getEndReading( ) const;
+    long getFare( ) const;
+    bool isCompeted( ) const;
+
+    void startTrip (long startReading);
+    double completeTrip (long endReading);
+
+    void display ( ) const;
+    string toString ( ) const;
+    void setDateFrom (Storable * s);
+  
+};
+
+#endif
