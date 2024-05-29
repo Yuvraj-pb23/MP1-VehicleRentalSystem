8ba6733206438347355c0fdd98efe751d79e1b4b
yuvraj <yuvrajjabbal@gmail.com>
2024-05-29 17:18:15 +0530

    29/5/24 5:18

--- table.h
+++ table.h
@@ -8,3 +8,26 @@
 #include <string>
 #include <fstream>
 
+using namespace std;
+template <class T> class Table {
+    private:
+
+      string fileName;
+      fstream fileStream;
+      vector<Storable *> * records = NULL;
+      T* getReferenceOfRecordForId (long recordId) const throw(NoSuchRecordError);
+
+      void writeTofile() throw (IOError);
+      const T* const addNewRecord(T data) throw (MemoryError,IOError);
+      void updateRecord ( T updateRecord) throw (IOError, NoSuchRecordError);
+
+    public:
+      Table (string fileName) throw (MemoryError);
+      -Table ();
+
+      long get NExtRecordId() const;
+
+      const T* const getRecordForId (long recordId) const throw (NoSuchRecordError);
+      friend class Database;    
+};
+#endif
