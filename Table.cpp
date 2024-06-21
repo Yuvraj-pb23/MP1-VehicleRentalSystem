2a37d5624bf45a3bc1fd30dca2cfeaec1094d5de
yuvraj <yuvrajjabbal@gmail.com>
2024-06-21 14:11:07 +0530

    new

--- Table.cpp
+++ Table.cpp
@@ -16,6 +16,7 @@ template <class T> long Table<T> :: getNextRecordId() const {
     return this -> records -> size() + 1;
 }
 
+
 template <class T> const T * const Table<T> :: addNewRecord(T record) throw (MemoryError,IOError) {
 
     T * newRecord = new T(record);
@@ -38,5 +39,66 @@ template <class T> const T * const Table<T> :: addNewRecord(T record) throw (Mem
 }
 
 template <class T> void Table<T> :: updateRecord (T updatedRecord) throw (IOError, NoSuchRecordError) {
-    
+    for (auto & record : *this->records) {
+        if (record->getRecordId() == updatedRecord.getRecordId()) {
+            T* pointerToRecord = dynamic_cast<T*> (record);
+            if (pointerToRecord) {
+                T oldRecord = T(*pointerToRecord);
+                record->setDataFrom(&updatedRecord);
+                try {
+                    this->writeTofile();
+                    return;
+                }
+                catch (IOError error) {
+                    record->setDataFrom(&oldRecord);
+                    throw;
+                }
+            }
+        }
+    }
+    throw NoSuchRecordError();
 }  
+
+template <class T> void Table<T> ::writeTofile() throw (IOError) {
+    this->fileStream.open(fileName, ios::out | ios::trunc);
+    if (!this->fileStream)
+    {
+        throw IOError();
+    }
+    for (auto& record : *records) {
+        fileStream << record->toString() << endl;
+    }
+    this->fileStream.close();
+}
+
+
+template <class T> const T* const Table<T> ::getRecordForId(long recordId) const throw (NoSuchRecordError) {
+    try {
+        return this->getReferenceOfRecordForId(recordId);
+    }
+    catch (NoSuchRecordError error) {
+        throw;
+    }
+}
+
+template<class T> T* Table<T> ::getReferenceOfRecordForId(long recordId) const throw (NoSuchRecordError) {
+    for (auto& record : *records) {
+        if (record->getRecordId() == recordId) {
+            return dynamic_cast<T*> *(record);
+        }
+    }
+    throw NoSuchRecordError();
+}
+
+template<classT> Table<T> :: ~Table(  ){
+    for (auto& record : *this->records) {
+        delete dynamic_cast<T*> (record);
+    }
+    this->records->clear();
+    this->records->shrink_to_fit();
+    delete this->records;
+}
+
+
+
+
