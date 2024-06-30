1498ea7a6bc19af80f9d88c17edfb65d489598a8
yuvraj <yuvrajjabbal@gmail.com>
2024-06-30 11:11:25 +0530

    new

---
+++ Database.h
@@ -0,0 +1,45 @@
+#ifndef database_h
+#define database_h
+
+#include "vehicle.h"
+#include "user.h"
+#include "trip.h"
+#include "table.cpp"
+#include "error.cpp"
+
+using namespace std;
+
+class Database {
+
+private:
+	Table<Vehicle>* vehicleTable;
+	Table<User>* userTable;
+	Table<Trip>* tripTable;
+
+	void fetchAllVehicles() throw (IOError, MemoryError);
+	void fetchAllUsers() throw (IOError, MemoryError);
+	void fetchAllTrips() throw (IOError, MemoryError);
+
+	void cleanUp();
+
+public:
+
+	Database() throw (MemoryError, IOError);
+
+	-Database();
+
+	const Table<Vehicle>* const getVehicleRef() const;
+	const Table<User>* const getUserRef() const;
+	const Table<Trip>* const getTripRef() const;
+
+	const Vehicle* const getVehicle(string regestrationNo) const throw (NoSuchRecordError);
+	const User* const getUser(string ContactNo) const throw (NoSuchRecordError);
+	const vector<const Vehicle*> getVehicle(Date startDate, Date endDate VehicleType type) const;
+
+	template <class T> void addNewRecord(T* record) throw (IOError, MemoryError);
+	template <class T> void UpdateRecord(T* record) throw (IOError, NoSuchRecordError);
+};
+
+#endif
+
+
