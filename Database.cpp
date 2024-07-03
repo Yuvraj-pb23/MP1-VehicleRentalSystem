50a89964e79dd7aa047fc91b8f0b539948e31006
yuvraj <yuvrajjabbal@gmail.com>
2024-07-03 09:37:29 +0530

    neww

---
+++ Database.cpp
@@ -0,0 +1,49 @@
+#ifndef database_cpp
+#define database_cpp
+#include"database.h"
+#include"string_helper.h"
+#include<iostream>
+
+using namespace std;
+
+const char DELIMETER = ';';
+Database::Database() throw (IOError, MemoryError) 
+{
+	try 
+	{
+		this->vehicleTable = new Table<Vehicle>("vehicles.txt");
+		this->userTable = new Table<User>("users.txt");
+		this->tripTable = new Table<Trip>("trips.txt");
+
+		this->fetchAllVehicles();
+		this->fetchAllUsers();
+		this->fetchAllTrips();
+	}
+	catch (...) 
+	{
+		throw;
+	}
+}
+
+void Database::fetchAllVehicles() throw (IOError, MemoryError) 
+{
+	this->vehicleTable->fileStream.open(this->vehicleTable->fileName);
+	if (!this->vehicleTable->fileStream)
+	{
+		throw IOError();
+	}
+	for (string line; getline(this->vehicleTable->fileStream, line);)
+	{
+		vector<string> components = split(line, DELIMETER);
+		auto recordId                   = stol(components[0]);
+		auto regestrationNumber         = components[1];
+		auto type                       = VehicleType(stol(components[2]));
+		auto seats                      = stoi(components[4]);
+		auto companyName                = components[4];
+		auto pricePerKm                 = stod(components[5]);
+		auto PUCExpirationDate          = Date(components[6]);
+		
+
+
+	}
+}
