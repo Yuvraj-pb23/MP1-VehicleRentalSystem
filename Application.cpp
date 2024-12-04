56575fb9c9e2d598caaa84ecd21776d08aad37cb
yuvraj <yuvrajjabbal@gmail.com>
2024-12-04 11:27:30 +0530

    nuu

--- Application.cpp
+++ Application.cpp
@@ -21,5 +21,81 @@ void Application :: gotoXY(int x,int y) const
 
 void Application::renderMenu()
 {
-    
+    unsigned char choice = 1;
+    while(true)
+    {
+        system("clear");
+        gotoXY(25, 4);
+        cout << "Select Any option from below";
+        gotoXY(25, 5);
+        cout << "!.Add new vehicle";
+        gotoXY(25, 6);
+        cout << "2. View vehicle details";
+        gotoXY(25, 7);
+        cout << "3. Edit vehicle details";
+        gotoXY(25, 8);
+        cout << "4. Add new user";
+        gotoXY(25, 9);
+        cout << "5. Add new trip";
+        gotoXY(25, 10);
+        cout << "6. View trip";
+        gotoXY(25, 11);
+        cout << "7. Start trip";
+        gotoXY(25, 12);
+        cout << "8. Complete trip";
+        gotoXY(25, 13);
+        cout << "9. Exit";
+
+        gotoXY(30, 15);
+        cout << "Enter your choice : ";
+
+        gotoXY(50, 15);
+        cin >> choice;
+        cin.ignore(numeric_limits<streamsize>::max(),'\n');
+       
+        switch(choice)
+        {
+           case '1':
+           this->renderAddNewVehicleMenu();
+           break;
+           case '2':
+           this->renderViewVehicleMenu();
+           break;
+           case '3':
+           this->renderEditVehicleMenu();
+           break;
+           case '4':
+           this->renderAddNewUserMenu();
+           break;
+           case '5':
+           this->renderAddNewTripMenu();
+           break;
+           case '6':
+           this->renderViewTripMenu();
+           break;
+           case '7':
+           this->renderStartTripMenu();
+           break;
+           case '8':
+           this->renderCompleteTripMenu();
+           break;
+           case '9':
+           this->clearMemory();
+           system("clear");
+           exit(EXIT_SUCCESS);
+           break;
+           default:
+           this->clearMemory();
+           system("clear");
+           exit(EXIT_SUCCESS);
+           break;
+           
+        }
+        
+    }
+}
+
+void Application :: renderAddNewVehicleMenu( ) const
+{
+  
 }
---
+++ Main.cpp
@@ -0,0 +1,9 @@
+#include"Application.h"
+#include<iostream>
+using namespace std;
+
+int main(int argc, const char * argv[])
+{
+ Application*appliation = new Application();
+ appliation->start();
+}
